487d77f2fc57629af85b3fa40d7bf1881fef798d
hhugoac <hhugoac@gmail.com>
2018-07-24 14:02:19 -0500

    Version with the JDBC connection & the first steps.

--- src/com/jdbcproject/controller/buscarProductoServlet.java
+++ src/com/jdbcproject/controller/buscarProductoServlet.java
@@ -1,11 +1,15 @@
 package com.jdbcproject.controller;
 
 import java.io.IOException;
+import java.sql.DriverManager;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import sun.rmi.transport.Connection;
 
 /**
  * Servlet implementation class buscarProductoServlet
@@ -28,7 +32,29 @@ public class buscarProductoServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String idProducto=request.getParameter("txtIdProducto");
 		response.setContentType("text/html");
-		response.getWriter().write("El Id del producto es: "+idProducto);
+		//Define the connection variables to the DB
+		String url="jdbc:mysql://localhost:3306/hugo"; //3306 port, /hugo databasename
+		String nameUser="root";
+		String password="root";
+		
+		//Put the database connections inside a try-catch statement
+		try
+		{
+			//Create an Driver object from JDBC package com.mysql.jdbc
+			//Driver objDriver=new Driver(); //Create an object DRiver but it is needed to import the module
+			Class.forName("com.mysql.jdbc.Driver").newInstance(); //Create an object without name
+			Connection conn=DriverManager.getConnection(url, nameUser, password);
+			
+			conn.close();
+		}
+		catch
+		{
+			
+		}
+		finally
+		{
+			
+		}
 	}
 
 }
